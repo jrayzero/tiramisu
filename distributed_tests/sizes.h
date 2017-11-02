5cddcd336f508deee317430d6324c3adf6c192fd
Jessica Ray <jray@csail.mit.edu>
2017-11-02 12:09:02 -0400

    Add sizes file.

---
+++ distributed_tests/sizes.h
@@ -0,0 +1,12 @@
+//
+// Created by Jessica Ray on 11/2/17.
+//
+
+#ifndef TIRAMISU_SIZES_H
+#define TIRAMISU_SIZES_H
+
+#define _ROWS ((uint64_t)80000)
+#define _COLS ((uint64_t)8750)
+#define _CHANNELS ((uint64_t)3)
+
+#endif //TIRAMISU_SIZES_H
