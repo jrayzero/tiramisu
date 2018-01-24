76e24983401a33b6479048bdb630622fd42205d2
Jessica Ray <jessica.ray@ll.mit.edu>
2018-01-23 19:05:14 -0500

    forgot file

--- blur/blur.h
+++ blur/blur.h
@@ -1,5 +1,6 @@
-#define GPU
-#define DIST
+//#define GPU
+#define COOP
+//#define DIST
 //#define CPU
 //#define CHECK
 
@@ -8,10 +9,10 @@
 #define COLS (int64_t)2000000//00
 #define RESIDENT (int64_t)400
 #define BLOCK_SIZE (int64_t)1000
-#define ITERS 10
+#define ITERS 1
 #else 
 #define ROWS (int64_t)16000
-#define COLS (int64_t)2000000
+#define COLS (int64_t)20000
 #define RESIDENT (int64_t)40
 #define BLOCK_SIZE (int64_t)100
 #define ITERS 10
